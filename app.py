425b0b3594319f0eb8c57b25dcb8d206d5d96a8f
Jared Patrick <jared.patrick@gmail.com>
2016-12-08 14:54:36 -0500

    fixed bad tornado import

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-import tornado.app
+import tornado.web
 import tornado.ioloop
 
 
-class HelloWorldHandler(tornado.app.RequestHandler):
+class HelloWorldHandler(tornado.web.RequestHandler):
     def get(self):
         self.write('Hello, World!')
 
 
 if __name__ == '__main__':
-    app = tornado.app.Application([v
+    app = tornado.web.Application([
         (r'^/', HelloWorldHandler)
     ])
     app.listen(80)
