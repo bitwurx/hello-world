7cac78a5c063bc5f671328d902fb22bf567d4813
Jared Patrick <jared.patrick@gmail.com>
2016-12-08 14:59:35 -0500

    changed server port to 8080

--- app.py
+++ app.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
     app = tornado.web.Application([
         (r'^/', HelloWorldHandler)
     ])
-    app.listen(80)
+    app.listen(8080)
     torando.ioloop.IOLoop.current().start()
